1918cbf9df86d5e35ff53f2a420a3f13122f3e70
Kenan Bölükbaşı <kenanbolukbasi@gmail.com>
2024-02-24 09:46:25 +0000

    Cleanup and documentation.

--- src/lisp/mopr/value.lisp
+++ src/lisp/mopr/value.lisp
@@ -5,101 +5,109 @@
 
 (eval-when (:compile-toplevel :load-toplevel :execute)
   (defconstant +value-type-list+
-    (list
-     ;; misc
-     '(:|bool| (unsigned-byte 1) :bool)
-     '(:|uchar| (unsigned-byte 8) :unsigned-char)
-     '(:|uint| (unsigned-byte 32) :unsigned-int)
-     '(:|int64| (signed-byte 64) :int64)
-     '(:|uint64| (unsigned-byte 64) :uint64)
+    '(;; misc
+      (:|bool|   (unsigned-byte 1)  :bool)
+      (:|uchar|  (unsigned-byte 8)  :unsigned-char)
+      (:|uint|   (unsigned-byte 32) :unsigned-int)
+      (:|int64|  (signed-byte 64)   :int64)
+      (:|uint64| (unsigned-byte 64) :uint64)
 
-     '(:|timecode| double-float :double)
-     '(:|string| string)
-     '(:|token| keyword)
-     '(:|asset| (or pathname string))
-     '(:|opaque| null) ; Only scalar allowed.
-     '(:|pathExpression| string) ; :path-expression
+      (:|timecode| double-float :double)
 
-     ;; int
-     '(:|int|  (signed-byte 32) :int)
-     '(:|int2| (signed-byte 32) :int)
-     '(:|int3| (signed-byte 32) :int)
-     '(:|int4| (signed-byte 32) :int)
+      (:|string| string)
 
-     ;; double
-     '(:|double|  double-float :double)
-     '(:|double2| double-float :double)
-     '(:|double3| double-float :double)
-     '(:|double4| double-float :double)
+      (:|token| keyword)
 
-     ;; float
-     '(:|float|  single-float :float)
-     '(:|float2| single-float :float)
-     '(:|float3| single-float :float)
-     '(:|float4| single-float :float)
+      (:|asset| (or pathname string))
 
-     ;; half
-     '(:|half|  short-float)
-     '(:|half2| short-float)
-     '(:|half3| short-float)
-     '(:|half4| short-float)
+      (:|opaque| null) ; Only scalar allowed.
 
-     ;; quat
-     '(:|quatd| double-float :double)
-     '(:|quatf| single-float :float)
-     '(:|quath| short-float)
+      (:|pathExpression| string) ; :path-expression
 
-     ;; matrix
-     '(:|matrix2d| double-float :double)
-     '(:|matrix3d| double-float :double)
-     '(:|matrix4d| double-float :double)))
+      ;; int
+      (:|int|  (signed-byte 32) :int)
+      (:|int2| (signed-byte 32) :int)
+      (:|int3| (signed-byte 32) :int)
+      (:|int4| (signed-byte 32) :int)
 
-  "Value type list data format:
-  - real-type
-  - (elt-type &optional dims)
-  - &optional cffi-type format-type")
+      ;; double
+      (:|double|  double-float :double)
+      (:|double2| double-float :double)
+      (:|double3| double-float :double)
+      (:|double4| double-float :double)
+
+      ;; float
+      (:|float|  single-float :float)
+      (:|float2| single-float :float)
+      (:|float3| single-float :float)
+      (:|float4| single-float :float)
+
+      ;; half
+      (:|half|  short-float)
+      (:|half2| short-float)
+      (:|half3| short-float)
+      (:|half4| short-float)
+
+      ;; quat
+      (:|quatd| double-float :double)
+      (:|quatf| single-float :float)
+      (:|quath| short-float)
+
+      ;; matrix
+      (:|matrix2d| double-float :double)
+      (:|matrix3d| double-float :double)
+      (:|matrix4d| double-float :double))
+
+    "Value type list data format:
+- real-type
+- elt-type
+- &optional cffi-type format-type
+
+If CFFI-TYPE is provided, an auto value transfer handler is registered.
+Otherwise, the type is not supported unless a custom handler is registered.
+If FORMAT-TYPE is provided, it is used to generate the function names 
+to call in value transfer handler. Otherwise, the real-type is used."))
 
 (defconstant +value-role-list+
-  (list
+  '(;; texCoord2x
+    (:|texCoord2d| . :|double2|)
+    (:|texCoord2f| . :|float2|)
+    (:|texCoord2h| . :|half2|)
 
-   ;; texCoord2x
-   '(:|texCoord2d| . :|double2|)
-   '(:|texCoord2f| . :|float2|)
-   '(:|texCoord2h| . :|half2|)
+    ;; texCoord3x
+    (:|texCoord3d| . :|double3|)
+    (:|texCoord3f| . :|float3|)
+    (:|texCoord3h| . :|half3|)
 
-   ;; texCoord3x
-   '(:|texCoord3d| . :|double3|)
-   '(:|texCoord3f| . :|float3|)
-   '(:|texCoord3h| . :|half3|)
+    ;; point3x
+    (:|point3d| . :|double3|)
+    (:|point3f| . :|float3|)
+    (:|point3h| . :|half3|)
 
-   ;; point3x
-   '(:|point3d| . :|double3|)
-   '(:|point3f| . :|float3|)
-   '(:|point3h| . :|half3|)
+    ;; vector3x
+    (:|vector3d| . :|double3|)
+    (:|vector3f| . :|float3|)
+    (:|vector3h| . :|half3|)
 
-   ;; vector3x
-   '(:|vector3d| . :|double3|)
-   '(:|vector3f| . :|float3|)
-   '(:|vector3h| . :|half3|)
+    ;; normal3x
+    (:|normal3d| . :|double3|)
+    (:|normal3f| . :|float3|)
+    (:|normal3h| . :|half3|)
 
-   ;; normal3x
-   '(:|normal3d| . :|double3|)
-   '(:|normal3f| . :|float3|)
-   '(:|normal3h| . :|half3|)
+    ;; color3x
+    (:|color3d| . :|double3|)
+    (:|color3f| . :|float3|)
+    (:|color3h| . :|half3|)
 
-   ;; color3x
-   '(:|color3d| . :|double3|)
-   '(:|color3f| . :|float3|)
-   '(:|color3h| . :|half3|)
+    ;; color4x
+    (:|color4d| . :|double4|)
+    (:|color4f| . :|float4|)
+    (:|color4h| . :|half4|)
 
-   ;; color4x
-   '(:|color4d| . :|double4|)
-   '(:|color4f| . :|float4|)
-   '(:|color4h| . :|half4|)
+    ;; misc
+    (:|frame4d| . :|matrix4d|)
 
-   ;; misc
-   '(:|frame4d| . :|matrix4d|)
-   '(:|group| . :|opaque|)))
+    (:|group| . :|opaque|)))
 
 (defun get-real-type (type-key)
   (or (cdr (assoc type-key +value-role-list+)) type-key))
