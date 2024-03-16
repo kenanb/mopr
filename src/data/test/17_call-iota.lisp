79a85f6fc2e743272f5bfb062300a8eaa004adb9
Kenan Bölükbaşı <kenanbolukbasi@gmail.com>
2024-03-16 11:17:05 +0000

    Replaced numeric literals in the iota test with variables.

--- src/data/test/17_call-iota.lisp
+++ src/data/test/17_call-iota.lisp
@@ -1,15 +1,19 @@
 ((:tree
   ("Grid10x10"))
  (:var :len () 1)
+ (:var :cell-size () 1)
+ (:var :x-dim () 10)
+ (:var :y-dim () 10)
+ (:var :z-dim () 0)
  (:var :grid-p ()
-  1 (0 10 10) #(2 0 1) :grid-points :drop)
+  :cell-size :z-dim :y-dim :x-dim :3list #(2 0 1) :grid-points :drop)
  (:iota :sine-args :time 20)
  (:prim ("Grid10x10")
   (:type :Mesh)
   (:call (:prop (:isa :Mesh :faceVertexCounts))
-   10 10 :grid-fv-counts nil :prop :make-prop)
+   :x-dim :y-dim :grid-fv-counts nil :prop :make-prop)
   (:call (:prop (:isa :Mesh :faceVertexIndices))
-   10 10 :cw :grid-fv-indices nil :prop :make-prop)
+   :x-dim :y-dim :cw :grid-fv-indices nil :prop :make-prop)
   (:call :sine-args
    :grid-p :len :time :grid-sine-y
    :dup :time (:isa :Mesh :points) :make-prop
